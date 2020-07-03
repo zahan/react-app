1cb574704e6454bcdb9e801fb892e86b09bb79d6
paul <this.is.paul.st@gmail.com>
2020-07-03 09:31:21 +0300

    compete

--- src/components/Users/users.jsx
+++ src/components/Users/users.jsx
@@ -9,7 +9,7 @@ const Users = (props) => {
             <span>{u.age}</span><br/>
             <span>{u.location.city}</span><br/>
             <span>{u.location.country}</span><br/>
-            {u.followed ? <button>Follow</button> : <button>Unfollow</button>}
+            {u.followed ? <button onClick={() => {props.unfollow(u.id)}} >Follow</button> : <button onClick={() => {props.follow(u.id)}}>Unfollow</button>}
             </div>)}
         </div>
     )
