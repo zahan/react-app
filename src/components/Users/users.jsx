00298c74ca23d50b265df1db34fd080e7fd4b7c2
paul <this.is.paul.st@gmail.com>
2020-07-04 18:22:16 +0300

    unit52

--- src/components/Users/users.jsx
+++ src/components/Users/users.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
+import * as axios from 'axios'
+import userDefaultPhoto from '../../assets/user.png'
 
 const Users = (props) => {
     
     if (props.users.length === 0) {
-    props.setUsers(
-        [
-            { id: 1, followed: true, fullName: 'Ekaterina', status: 'Hello everybody', age: 27, location: { city: 'Saint-Petersburg', country: 'Russia' } },
-            { id: 2, followed: true, fullName: 'Eva', status: 'Where is my milk', age: 0, location: { city: 'Saint-Petersburg', country: 'Russia' } },
-            { id: 3, followed: false, fullName: 'Pavel', status: 'Get to the choppa', age: 31, location: { city: 'Saint-Petersburg', country: 'Russia' } }
-        ]
-    )
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        })
+            
  } 
 
     return (
         <div>
             {props.users.map(u => <div key={u.id}>
-                <span>{u.fullName}</span><br />
+                <img width='40' src={u.photos.small != null ? u.photos.small: userDefaultPhoto } alt='user'/>
+                <span>{u.name}</span><br />
                 <span>{u.status}</span><br />
-                <span>{u.age}</span><br />
-                <span>{u.location.city}</span><br />
-                <span>{u.location.country}</span><br />
+                <span>{u.id}</span><br />
+                {/* <span>{u.location.city}</span><br />
+                <span>{u.location.country}</span><br /> */}
                 {u.followed ? <button onClick={() => { props.unfollow(u.id) }} >Follow</button> : <button onClick={() => { props.follow(u.id) }}>Unfollow</button>}
             </div>)}
         </div>
