4d5f3280437722b5194afa251cbec16ff3c42f01
paul <this.is.paul.st@gmail.com>
2020-07-05 12:01:19 +0300

    u51-3

--- src/components/Users/usersCl.jsx
+++ src/components/Users/usersCl.jsx
@@ -5,18 +5,18 @@ import userDefaultPhoto from '../../assets/user.png'
 
 class UsersCl extends React.Component {
 
-    GetUsers = () => {
-        if (this.props.users.length === 0) {
-
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    constructor(props){
+        super(props)
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
                 this.props.setUsers(response.data.items)
             })
-        }
     }
+
+   
 
     render () {
     return <div>
-        <button onClick={this.GetUsers}>Get users</button>
+        {/* <button onClick={this.GetUsers}>Get users</button> */}
         {this.props.users.map(u => <div key={u.id}>
             <img width='40' src={u.photos.small != null ? u.photos.small : userDefaultPhoto} alt='user' />
             <span>{u.name}</span><br />
