2f171a13faf08834dfa03d971f0a19a98ac26861
paul <this.is.paul.st@gmail.com>
2020-07-16 22:27:50 +0300

    u59-2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
+import ProfileContainer from './components/Profile/profileContainer';
 import { Route, BrowserRouter } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
@@ -25,7 +25,7 @@ const App = (props) => {
           <Navbar />
 
           <div className='app-wrapper-content'>
-            <Route path='/profile' render={() => <Profile />} />
+            <Route path='/profile' render={() => <ProfileContainer />} />
             <Route path='/dialogs' render={() => <DialogsContainer />} />
             <Route path='/users' render={() => < UsersContainer /> } />
             <Route path='/news' component={News} />
---
+++ src/components/Profile/profileContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Profile from './Profile'
+
+class ProfileContainer extends React.Component {
+
+    render() {
+        return <Profile {...this.props} />
+    }
+}
+
+export default ProfileContainer
