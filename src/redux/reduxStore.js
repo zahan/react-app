f1d0403bc53d270f951ac5ccee9dadeacbdf5948
paul <this.is.paul.st@gmail.com>
2020-07-25 12:18:51 +0300

    u75-3

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -4,12 +4,14 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunkMiddleware from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form'
 
 let reducersPack = combineReducers({
     WallPosts: postReducer,
     MessagesDia: messageReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
  
 let store = createStore(reducersPack, applyMiddleware(thunkMiddleware))
